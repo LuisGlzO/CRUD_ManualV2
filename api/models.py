e7f49ed5d31bd159ac6a554138205daa1721dfd7
LuisGlzO <luisvip16@gmail.com>
2021-01-27 12:38:10 -0600

    ForeignKey agregada con exito

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Salones(models.Model):
-    id = models.AutoField(primary_key = True)
+    #id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length = 30)
     edificio = models.CharField(max_length = 30)
     capacidad = models.PositiveIntegerField()
@@ -11,11 +11,11 @@ class Salones(models.Model):
         return self.nombre
 
 class Persona(models.Model):
-    id = models.AutoField(primary_key = True)
+    #id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length = 100)
     apellido = models.CharField(max_length = 100)
     correo = models.EmailField(max_length = 200)
-    salon = models.ForeignKey(Salones, on_delete=models.CASCADE)
+    salonAsignado = models.ForeignKey(Salones, on_delete=models.CASCADE, null = True, blank = True, default=None)
 
     def __str__(self):
         persona = self.nombre + self.apellido
